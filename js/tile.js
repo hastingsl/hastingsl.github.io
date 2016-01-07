08dc85365ce62216ab0476c630e8d8695e070367
hastingsl <lhastingsa@gmail.com>
2016-01-06 20:49:50 -0500

    Finished tile animation on splash section, completed everything but blog pages.

--- js/tile.js
+++ js/tile.js
@@ -1,33 +1,67 @@
 /* JavaScript to generate and darken/lighten the colored tiles at random. */
 
-// Object representing tile color:
-function tile_color(background, border) {
-  this.background = background;
-  this.border = border;
-}
-
-// Array of possible tile colors:
-var tile_colors = [];
-// Add colors to array:
-// Dark blue:
-tile_colors.push(new tile_color("rgb(51, 122, 183)", "rgba(51, 122, 183, 0.5)"));
-// Green:
-tile_colors.push(new tile_color("rgb(92, 184, 92)", "rgba(92, 184, 92, 0.5)"));
-// Teal:
-tile_colors.push(new tile_color("rgb(91, 192, 222)", "rgba(91, 192, 222, 0.5)"));
-// Orange:
-tile_colors.push(new tile_color("rgb(240, 173, 78)", "rgba(240, 173, 78, 0.5)"));
-// Red:
-tile_colors.push(new tile_color("rgb(217, 83, 79)", "rgba(217, 83, 79, 0.5)"));
-
 $(function(){
-7
-  // Generate tiles:
-  for(var i = 0; i < 100; i++) {
-    $("#tile-holder").append('<div class="tile" id="id-' + i + '"></div>');
-    var color_index = Math.floor(Math.random() * (tile_colors.length));
-    var color_obj = tile_colors[color_index];
-    $("#id-" + i).css("background-color", color_obj.background);
+  generate_tiles();
+  setInterval(animate_tiles, 1000);
+});
+
+function animate_tiles() {
+
+  // Return dark tiles to normal:
+  $(".darken").each(function(index) {
+    $("#" + $(this).attr("id")).removeClass("darken");
+  });
+
+  // Return light tiles to normal:
+  $(".lighten").each(function(index) {
+    $("#" + $(this).attr("id")).removeClass("lighten");
+  });
+
+  for(var i = 0; i < 10; i++) {
+    // Lighten random tile:
+    lighten();
+    // Darken random tile:
+    darken();
   }
 
-});
+}
+
+function darken() {
+  var item_id = "#overlay-" + get_random_num(100);
+  console.log(item_id);
+  if($(item_id).hasClass("darken") || $(item_id).hasClass("lighten")) {
+    darken();
+  }
+  else {
+    console.log("something");
+    $(item_id).addClass("darken");
+  }
+}
+
+function generate_tiles() {
+  // Array of possible tile colors:
+  var tile_colors = ["rgb(51, 122, 183)", "rgb(92, 184, 92)", "rgb(91, 192, 222)", "rgb(240, 173, 78)", "rgb(217, 83, 79)"];
+  for(var i = 0; i < 100; i++) {
+    // Adds grid of tiles:
+    $("#tile-holder").append('<div class="tile" id="tile-' + i + '"></div>');
+    // Adds grid of overlay tiles to lighten/darken:
+    $("#overlay-grid").append('<div class="overlay-tile tile" id="overlay-' + i + '"></div>');
+    var color = tile_colors[get_random_num(tile_colors.length)];
+    // Randomly assigns color to each tile:
+    $("#tile-" + i).css("background-color", color);
+  }
+}
+
+function get_random_num(upper_limit) {
+  return Math.floor(Math.random() * upper_limit);
+}
+
+function lighten() {
+  var item_id = "#overlay-" + get_random_num(100);
+  if($(item_id).hasClass("darken") || $(item_id).hasClass("lighten")) {
+    lighten();
+  }
+  else {
+    $(item_id).addClass("lighten");
+  }
+}
