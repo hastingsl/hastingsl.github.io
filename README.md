9360d44eaf7b4e0ddbf03177ebf5df925800ac1a
Lauren Hastings <Lauren Hastings>
2016-04-29 20:27:53 -0400

    Installed JS linter and edited JS up to Standard style guidelines.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ I made this site from scratch using Jekyll, HTML, CSS, JavaScript, minimal jQuer
 The main personal page is complete, but a blog section will be added in the near future.
 
 Feel free to file an issue or send me a message if you have any suggestions for improvements, notice any spelling errors, or have issues viewing it on different devices.
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
--- js/tile.js
+++ js/tile.js
@@ -1,45 +1,33 @@
-// JavaScript responsible for main page flashing tiles.
-
-$(function() {
-
-  // Randomly assigns colors to tiles:
-  var colors = ["#5bc0de", "#337ab7", "#5cb85c", "#f0ad4e", "#d9534f"];
-  for(var i = 0; i < 100; i++)
-    $("#tile-" + i).css("background-color", colors[getRandomNumber(colors.length)]);
-
-  setInterval(animate, 1000);
-
-});
-
-function animate() {
-
-  // Removes the lighten and darken classes on elements possessing these classes.
-  remove("dark");
-  remove("light");
-
-  // Darkens and lightens 20 tiles.
-  for(var i = 0; i < 10; i++) {
-    changeColor("dark");
-    changeColor("light");
+// JavaScript responsible for main page flashing tiles
+$(function () {
+  var colors = ['#5bc0de', '#337ab7', '#5cb85c', '#f0ad4e', '#d9534f']
+  for (var i = 0; i < 100; i++) {
+    $('#tile-' + i).css('background-color', colors[getRandomNumber(colors.length)])
   }
+  setInterval(animate, 1000)
+})
 
+function animate () {
+  // Removes the lighten and darken classes on elements possessing these classes.
+  remove('dark')
+  remove('light')
+  // Darkens and lightens 20 tiles.
+  for (var i = 0; i < 10; i++) {
+    changeColor('dark')
+    changeColor('light')
+  }
 }
 
-/* Randomly chooses an overlay tile to darken or lighten by adding the class
-  passed to the function. */
-function changeColor(transition) {
-  $("#overlay-" + getRandomNumber(100)).addClass(transition);
+function changeColor (transition) {
+  $('#overlay-' + getRandomNumber(100)).addClass(transition)
 }
 
-/* Generates a random integer less than the upper limit passed to the function. */
-function getRandomNumber(upperLimit) {
-  return Math.floor(Math.random() * upperLimit);
+function getRandomNumber (upperLimit) {
+  return Math.floor(Math.random() * upperLimit)
 }
 
-/* Iterates over every element possessing the class name passed to the function
-  and removes the class. */
-function remove(className) {
-  $("." + className).each(function(index) {
-    $("#" + $(this).attr("id")).removeClass(className);
-  });
+function remove (className) {
+  $('.' + className).each(function (index) {
+    $('#' + $(this).attr('id')).removeClass(className)
+  })
 }
